543f3ff7543dc0aa27f8bdc4b1187cff071f3e49
Travis Haley <travis@haleycomputersolutions.com>
2026-06-14 15:56:24 -0600

    feat: export the authoring surface (new, new change, fix, init, templates)

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ const { loadConfig } = require('./config');
 const { formatResults } = require('./formatter');
 const { renderContract, renderManagedBlock, extractManagedBlock } = require('./renderer');
 const { BASE_CONFIG } = require('./base-config');
+const { renderCapabilitySpec, renderChangeRecord, slugToTitle, stripDate } = require('./templates');
+const indexTable = require('./index-table');
+const { toJsonReport } = require('./json-output');
+const { newCapability } = require('./commands/new');
+const { newChange } = require('./commands/new-change');
+const { fix } = require('./commands/fix');
+const { init } = require('./commands/init');
 
 module.exports = {
   lint,
@@ -14,4 +21,14 @@ module.exports = {
   renderManagedBlock,
   extractManagedBlock,
   BASE_CONFIG,
+  renderCapabilitySpec,
+  renderChangeRecord,
+  slugToTitle,
+  stripDate,
+  indexTable,
+  toJsonReport,
+  newCapability,
+  newChange,
+  fix,
+  init,
 };
