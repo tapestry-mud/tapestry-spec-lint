2a0edee543b434c24b221b40f8fe7ba61ed4bc9c
Travis Haley <travis@haleycomputersolutions.com>
2026-06-14 03:15:04 -0600

    feat: public API surface, remove smoke test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 'use strict';
 
-module.exports = {};
+const { lint } = require('./engine');
+const { loadConfig } = require('./config');
+const { formatResults } = require('./formatter');
+const { renderContract, renderManagedBlock, extractManagedBlock } = require('./renderer');
+const { BASE_CONFIG } = require('./base-config');
+
+module.exports = {
+  lint,
+  loadConfig,
+  formatResults,
+  renderContract,
+  renderManagedBlock,
+  extractManagedBlock,
+  BASE_CONFIG,
+};
--- tests/smoke.test.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-test('smoke: jest is wired', () => {
-  expect(1 + 1).toBe(2);
-});
